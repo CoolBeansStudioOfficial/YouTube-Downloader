534f685b10703935ea3b773f18dbe885c1041ba2
CoolBeansStudio64 <leviwarrendumas91908@gmail.com>
2024-07-24 20:19:38 -0500

    Wow! a button?

--- YouTube Downloader/MainWindow.axaml.cs
+++ YouTube Downloader/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+using System.Diagnostics;
+using Tmds.DBus.Protocol;
 
 namespace YouTube_Downloader
 {
@@ -7,6 +11,11 @@ namespace YouTube_Downloader
         public MainWindow()
         {
             InitializeComponent();
+        }
+       
+        public void DownloadButton(object sender, RoutedEventArgs args)
+        {
+            Debug.WriteLine("Button clicked!");
         }
     }
 }
